631197d0c155eaefb522ec5734b0f4abc8139663
Valeriy Panafidin <swvalerianJava@yandex.ru>
2021-05-14 23:26:20 +0500

    слой репозиторий готов к работе!

--- src/main/java/com/swvalerian/crud/AppRunner.java
+++ src/main/java/com/swvalerian/crud/AppRunner.java
@@ -2,9 +2,12 @@ package com.swvalerian.crud;
 
 import com.swvalerian.crud.model.Developer;
 import com.swvalerian.crud.model.Skill;
+import com.swvalerian.crud.model.Team;
 import com.swvalerian.crud.repository.DeveloperRepository;
 import com.swvalerian.crud.repository.JavaIODevRepImpl;
+import com.swvalerian.crud.repository.JavaIOTeamRepImpl;
 import com.swvalerian.crud.repository.SkillRepository;
+import com.swvalerian.crud.view.MainView;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -13,9 +16,51 @@ import java.util.List;
 public class AppRunner {
     public static void main(String[] args) throws SQLException {
 
+        MainView mainView = new MainView();
+
+        mainView.showAll();
+
+        /*JavaIOTeamRepImpl teamRep = new JavaIOTeamRepImpl();
+
+        List<Team> teamList = teamRep.getAll();
+
+        // test1
+        System.out.println("\nПроверка метода getAll");
+        for (Team team:teamList) {
+            System.out.println(team + "\n");
+        }
+
+        // test2
+        System.out.println("\nПроверим метод getId = 1 -> " + teamRep.getId(1l));
+
+        // test3
+        System.out.println("\nПроверим метод save");
+
+        List<Developer> developerList = new JavaIODevRepImpl().getAll();
+        developerList.remove(1);
+        developerList.remove(5);
 
 
+        Team team = new Team(10,"", developerList);// неважно каким будет ID
+        // teamRep.save(team);
+        teamRep.deleteById(12l);
+*/
+        /*System.out.println(teamRep.getId(11l));
 
+        System.out.println("Проверим метод - удаление: ");
+        teamRep.deleteById(11l);
+
+        System.out.println(teamRep.getId(10l));*/
+
+        // test4
+        /*System.out.println("\n проверим метод Update");
+        Team teamUpdate = new Team(10,"Extra", developerList);// неважно каким будет ID
+        developerList.remove(2);
+        developerList.remove(3);
+
+        teamRep.update(teamUpdate);
+
+        System.out.println(teamRep.getId(10l));*/
 
         /*
         // тест DevRepo
--- src/main/java/com/swvalerian/crud/repository/JavaIOTeamRepImpl.java
+++ src/main/java/com/swvalerian/crud/repository/JavaIOTeamRepImpl.java
@@ -12,42 +12,42 @@ import java.util.stream.Collectors;
 
 // Team(id, name, List<Developer> developers)
 public class JavaIOTeamRepImpl implements TeamRepository{
-    final private File file = new File("src\\main\\resources\\files\\teams.txt");
     //static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; // устарело. драйвер подгружается автоматом
     static private final String DATABASE_URL = "jdbc:mysql://localhost:3306/swvalerian";
     static private final String User = "root";
     static private final String Password = "QWERTgfdsa1980";
 
+    // JDBC!
     private List<Team> getListFFTeam() {
         List<Team> teamList = new ArrayList<>();
-
-        List<Developer> developerList = new ArrayList<>(); // этот список постоянно будет изменяться, возможно надо будет перенести в метод ниже
-
         Connection connection = null; // Интерфейс для соединения с менеджером драйверов БД
         PreparedStatement preparedStatement = null; // А этот содержит методы подтверждения запросов
 
         try {
             connection = DriverManager.getConnection(DATABASE_URL, User, Password);
-            String SQL = "select * from team";
-            // подготовили запрос
+            // подготовили запрос: достаточно получить ID участников, чтоб в дальнейшем составить их список
+            String SQL = "select id, Name, Dev_id FROM Team\n" +
+                        "JOIN Team_Developers\n" +
+                        "ON Team.Id = Team_Developers.Team_Id;";
+
             preparedStatement = connection.prepareStatement(SQL, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             // выполняем запрос
             ResultSet rS = preparedStatement.executeQuery();
-            int prevId = 1; // для сравнения ID, если тот же девелопер, тогда список Skill заполняем дальше
+            int prevId = 1; // для сравнения ID, если team та же самая, тогда список Developers заполняем дальше
 
             while (rS.next()) {
-                List<Skill> skillList = new ArrayList<>();
-                String firstName = rS.getString("firstName");
-                String lastName = rS.getString("lastName");
-                int id = rS.getInt("Id");
+                // этот список постоянно будет изменяться, т.е. создавать надо заново, поэтому обьявление списка именно тут
+                List<Developer> devList = new ArrayList<>();
+                int id = rS.getInt("id");
+                String name = rS.getString("Name");
 
                 while (id == prevId) {
-                    skillList.add(new Skill(rS.getInt("Skill_Id"), rS.getString("Skill")));
+                    long Dev_id = rS.getLong("Dev_id");
+                    devList.add(new JavaIODevRepImpl().getId(Dev_id));
                     if (rS.next()) { id = rS.getInt("Id");
                     } else { break; }
                 }
-
-                devList.add(new Developer(prevId, firstName, lastName, skillList));
+                teamList.add(new Team(prevId, name, devList));
                 prevId = id; // изменили значение на следующее.
                 // теперь нужно вернуть указатель на предыдущий маркер, т.к. при следующей итерации он вновь сдвинется
                 rS.previous();
@@ -59,83 +59,145 @@ public class JavaIOTeamRepImpl implements TeamRepository{
         } catch (SQLException e) {
             System.err.println("Ошибка в работе с БД в ДевРеп -> метод getListFFD");
         }
-
         return teamList;
     }
 
-    private Team convertStringToTeam(String s) {
-        s = s.substring(0, s.length()-1); // получили строку без последнего знака "/"
-        String[] str = s.split(",");
-
-        List<Developer> devList = new ArrayList<>();
-        int i = 2;
-        while (i < str.length) {
-            devList.add(new JavaIODevRepImpl().getId(Long.decode(str[i])));
-            i++;
-        }
-
-        String name = str[1];
-        Integer id = Integer.decode(str[0]);
-        return new Team(id, name, devList);
-    }
-
+    // JDBC!
     @Override
     public List<Team> getAll() {
         return getListFFTeam();
     }
 
+    // JDBC!
     @Override
     public Team getId(Long id) {
         return getListFFTeam().stream().filter( s -> s.getId().equals(id.intValue())).findFirst().orElse(null);
     }
 
+    // JDBC!
     @Override
     public List<Team> update(Team team) {
-        List<Team> teamList = getListFFTeam();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
 
-        file.delete();
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL, User, Password);
+            // подготовим запрос для изменения записи в таблице
+            String SQL = "UPDATE Team SET Name=? WHERE Id = ?;";
+            preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setString(1, team.getName());
+            preparedStatement.setInt(2, team.getId());
+            // выполняем запрос
+            preparedStatement.executeUpdate();
 
-        return teamList.stream().peek( s ->
-        {
-            if (s.getId().equals(team.getId())) {
-                s.setName(team.getName());
+            // теперь нам надо изменить данные о девелоперах в команде - тут по хитрому поступим.
+            // Сначало сотрем все старые данные об умениях (и неважно где они в таблице)
+            // а после добавим новые данные, в конец таблицы, тоже не важно - где они хранятся.
+            SQL = "DELETE FROM Team_Developers WHERE Team_id = ?;";
+            preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setLong(1, team.getId());
+            // удаляем
+            preparedStatement.executeUpdate();
+
+            //а теперь добавим данные о сотаве команды в таблицу Team_Developers
+            List<Developer> developerList = team.getDevelopers(); // получим список
+
+            SQL = "INSERT INTO Team_Developers (Team_Id, Dev_Id) values(?,?);";
+            preparedStatement = connection.prepareStatement(SQL);
+
+            for ( int i = 0; i < developerList.size(); i++) {
+                preparedStatement.setInt(1, team.getId());
+                preparedStatement.setInt(2, developerList.get(i).getId());
+                preparedStatement.addBatch(); // пишем запросы в пакет
             }
-            writeBuf(s);
-        }).collect(Collectors.toList());
+            preparedStatement.executeBatch(); // пакетный запуск запросов.
+
+            // закроем открытые соединения
+            connection.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            System.err.println("Ошибка работы с БД в методе TeamRep.UpDate");
+        }
+        return getListFFTeam();
     }
 
-    // приватный метод, уменьшаем основной код
+    // JDBC!
     private void writeBuf(Team team) {
-        try (OutputStream out = new FileOutputStream(file, true);
-             BufferedWriter bufWrite = new BufferedWriter(new OutputStreamWriter(out)))
-        {
-            bufWrite.write(convertTeamToString(team)); // получаем наш формат строки в файле, куда и будет сделана запись
-            bufWrite.flush(); // из буфера в файл за раз!
+        Connection connection = null; // Интерфейс для соединения с менеджером драйверов БД
+        PreparedStatement preparedStatement = null; // А этот содержит методы подтверждения запросов
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL, User, Password);
+
+            String name = team.getName();
+            List<Developer> developerList = team.getDevelopers();
+
+            // подготовим запрос на добавление нового элемента в таблицу
+            String SQL = "INSERT INTO Team (Name) values(?);"; // у нас автоинкремент, ID автоматом проставится
+
+            preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setString(1, name);
+            preparedStatement.executeUpdate();
+
+            SQL = "SELECT last_insert_id();"; // надо узнать номер инкремента, чтоб корректно добавить ID в Team_Dev
+            ResultSet resultSet = preparedStatement.executeQuery(SQL);
+            resultSet.next();
+            int Team_Id = resultSet.getInt(1); // получили ID вновь добавленной команды
+
+            // теперь запишем developers в таблицу Team_developers
+            SQL = "INSERT INTO Team_Developers (Team_Id, Dev_Id) values(?,?);";
+            preparedStatement = connection.prepareStatement(SQL);
+
+            for ( int i = 0; i < developerList.size(); i++) {
+                preparedStatement.setInt(1,Team_Id);
+                preparedStatement.setInt(2, developerList.get(i).getId());
+                preparedStatement.addBatch(); // пишем запросы в пакет
+            }
+            preparedStatement.executeBatch(); // пакетный запуск запросов.
+
+            // закроем открытые соединения
+            connection.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            System.err.println("Ошибка работы с БД в методе TeamRep.WriteBuf");
         }
     }
 
-    private String convertTeamToString(Team team) {
-        return team.getId().toString() + "," + team.getName() + "/" +"\n";
-    }
-
+    // JDBC!
     @Override
     public Team save(Team team) {
         writeBuf(team);
         return team;
     }
 
+    // JDBC!
     @Override
     public void deleteById(Long id) {
-        List<Team> teamList = getListFFTeam();
-        teamList.removeIf(s -> s.getId().equals(id.intValue()));
+        Connection connection = null; // Интерфейс для соединения с менеджером драйверов БД
+        PreparedStatement preparedStatement = null; // А этот содержит методы подтверждения запросов
 
-        file.delete();
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL, User, Password);
 
-        teamList.forEach(this::writeBuf);
+            // подготовим запрос для удаления записи из таблицы
+            String SQL = "DELETE FROM Team WHERE Id = ?;";
+            preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setLong(1, id);
+            // выполняем запрос
+            preparedStatement.executeUpdate();
+
+            // подготовим запрос для удаления записей из таблицы Team_Developers
+            SQL = "DELETE FROM Team_Developers WHERE Team_id = ?;";
+            preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setLong(1, id);
+            // выполняем запрос
+            preparedStatement.executeUpdate();
+
+            // закроем открытые соединения
+            connection.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            System.err.println("Ошибка работы с БД в методе Team.DeleteByID");
+        }
     }
 }
